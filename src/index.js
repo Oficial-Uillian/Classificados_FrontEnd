2695599537a037d8cde2916ca44f49097f3775bb
Uillian <uilli@LAPTOP-UH1M19MR>
2021-03-10 15:36:33 -0300

    subindo login

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import Login from '../src/paginas/login'
+import ResetarSenha from '../src/paginas/login/resetarsenha' 
+
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+
+const rotas = (
+  <Router>
+    <Switch>
+    <Route path='/' exact component={Login}/>
+    <Route path='resetarsenha' component={ResetarSenha} />
+    </Switch>
+  </Router>
+)
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+ rotas,
   document.getElementById('root')
 );
 
